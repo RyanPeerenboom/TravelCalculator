ec0cfb00dcfab8629d7855821e3dc8a39b86cb9e
RyanPeerenboom <50986066+RyanPeerenboom@users.noreply.github.com>
2019-10-22 18:05:07 -0500

    Added Controller and TravelCalculator.java

---
+++ TravelCalculator/src/Controller.java
@@ -0,0 +1,54 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+	public class Controller {
+
+	    @FXML
+	    private Label InputCurrencyLabel;
+
+	    @FXML
+	    private Label InputCurrencyDropdownLabel;
+
+	    @FXML
+	    private Label OutputCurrencyDropdownLabel;
+
+	    @FXML
+	    private TextField InputCurrencyField;
+
+	    @FXML
+	    private TextField OutputCurrencyField;
+
+	    @FXML
+	    private Label OutputCurrencyFieldLabel;
+
+	    @FXML
+	    private ComboBox<?> InputCurrencyDropdown;
+
+	    @FXML
+	    private ComboBox<?> OutputCurrencyDropdown;
+
+	    @FXML
+	    private Button CalculateButton;
+
+	    @FXML
+	    void CalculateClick(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void InputCurrencyPopulate(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void OutputCurrencyPopulate(ActionEvent event) {
+
+	    }
+
+	}
+
+
