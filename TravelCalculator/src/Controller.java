211928bebf898350faed6fa2c60bc799d955042a
RyanPeerenboom <50986066+RyanPeerenboom@users.noreply.github.com>
2019-10-22 20:35:46 -0500

    Removed unnecessary Currency class, made Conversion object which holds input/ouput types and amounts to be used in controller class

--- TravelCalculator/src/Controller.java
+++ TravelCalculator/src/Controller.java
@@ -1,3 +1,13 @@
+import java.util.List;
+
+import javax.money.CurrencyUnit;
+import javax.money.MonetaryAmount;
+import javax.money.NumberValue;
+import javax.money.convert.ConversionContext;
+import javax.money.convert.CurrencyConversion;
+import javax.money.convert.ExchangeRate;
+import javax.money.convert.ExchangeRateProvider;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -5,7 +15,14 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-	public class Controller {
+	public class Controller extends Conversion implements ExchangeRate, CurrencyConversion{
+		
+		public Controller(MonetaryAmount inputCurrencyValue, MonetaryAmount outputCurrencyValue, ExchangeRate exchangeRate,
+				CurrencyUnit inputCurrencyType, CurrencyUnit outputCurrencyType) {
+			
+			super(inputCurrencyValue, outputCurrencyValue, exchangeRate, inputCurrencyType, outputCurrencyType);
+			
+		}
 
 	    @FXML
 	    private Label InputCurrencyLabel;
@@ -36,6 +53,13 @@ import javafx.scene.control.TextField;
 
 	    @FXML
 	    void CalculateClick(ActionEvent event) {
+	    	double InputCurrencyValue = Double.parseDouble(InputCurrencyField.getText());
+	    	/*double outputCurrencyValue = Double.parseDouble(OutputCurrencyField.getText());*/
+	    	
+	    	String inputCurrencyType = InputCurrencyDropdown.toString();
+	    	String outputCurrencyType = OutputCurrencyDropdown.toString();
+	    	
+	    	
 	    	
 	    }
 
@@ -49,6 +73,156 @@ import javafx.scene.control.TextField;
 	    	
 	    }
 
+		@Override
+		public MonetaryAmount apply(MonetaryAmount amount) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public ExchangeRate getExchangeRate(MonetaryAmount sourceAmount) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public ExchangeRateProvider getExchangeRateProvider() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public ConversionContext getContext() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public CurrencyUnit getBaseCurrency() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public CurrencyUnit getCurrency() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public NumberValue getFactor() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public List<ExchangeRate> getExchangeRateChain() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+	    
+	   /* @Override
+		public ExchangeRate getExchangeRate(MonetaryAmount inputCurrencyValue) {
+			
+			if (inputCurrencyType.equals(getBaseCurrency()) && outputCurrencyType.equals(getCurrency()) ) {
+				  
+				exchangeRate = getExchangeRate(inputCurrencyValue);
+				
+			}
+
+			return exchangeRate; 
+		}
+		
+		@Override
+		public CurrencyUnit getBaseCurrency() {
+			
+			return inputCurrencyType;
+		}
+		
+		@Override
+		public CurrencyUnit getCurrency() {
+			
+			return outputCurrencyType;
+		}
+		
+		
+		@Override
+		public MonetaryAmount apply(MonetaryAmount inputCurrencyValue) {
+			
+			
+			
+			
+			
+			return outputCurrencyValue;
+		}
+		
+		
+		@Override
+		public ConversionContext getContext() {
+			
+			return null;
+		}
+
+		
+
+		
+		
+
+		@Override
+		public NumberValue getFactor() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public List<ExchangeRate> getExchangeRateChain() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		
+
+		
+
+		@Override
+		public ExchangeRateProvider getExchangeRateProvider() {
+			// TODO Auto-generated method stub
+			return null;
+		} */
+		
+		
+		
+		
+		
+		
+		
+		//method for calculation of 'left to right' (divide) conversion
+	/*	public MonetaryAmount leftToRightDivide() {
+			
+			
+			outputCurrencyValue = inputCurrencyValue.multiply(getFactor(inputCurrencyValue));
+			
+			return outputCurrencyValue;
+			
+		}
+		
+		*/
+		
+		/* //method for calculation of 'right to left' (multiply) conversion
+		public MonetaryAmount rightToLeftMultiply() {
+			
+			outputCurrencyValue = inputCurrencyValue.multiply((Number) getExchangeRateChain().get(1));
+			
+			return outputCurrencyValue;
+			
+		}
+		*/
+
+		
+
 	}
+
+
+	
 
 
--- TravelCalculator/src/Conversion.java
+++ TravelCalculator/src/Conversion.java
@@ -1,126 +1,32 @@
-import javax.money.convert.*;
-import javax.money.convert.RateType;
 import javax.money.convert.ExchangeRate;
-
-import java.util.List;
-
 import javax.money.*;
 //class used to manipulate a currency object
-public class Conversion extends Currency implements ExchangeRate, CurrencyConversion{
+public abstract class Conversion {
 	
+	//currency object variables (common to all currency objects)
+	protected MonetaryAmount inputCurrencyValue;
+	protected MonetaryAmount outputCurrencyValue;
+	protected CurrencyUnit inputCurrencyType;
+	protected CurrencyUnit outputCurrencyType;
 	protected ExchangeRate exchangeRate;
-	protected CurrencyConversion conv;
 	
 	
-	//constructor for conversion object (inherits from currency)
+	
 	public Conversion(MonetaryAmount inputCurrencyValue, MonetaryAmount outputCurrencyValue, ExchangeRate exchangeRate,
 			CurrencyUnit inputCurrencyType, CurrencyUnit outputCurrencyType) {
-		super(inputCurrencyValue, outputCurrencyValue, inputCurrencyType, outputCurrencyType);
 		
+		this.inputCurrencyType = inputCurrencyType;
+		this.outputCurrencyType = outputCurrencyType;
+		this.inputCurrencyValue = inputCurrencyValue;
+		this.outputCurrencyValue = outputCurrencyValue;
 		this.exchangeRate = exchangeRate;
+	
 	}
+}
+
 	
 
 	//method for assignment of exchangeRateValue;	
 	
 	
-	@Override
-	public ExchangeRate getExchangeRate(MonetaryAmount inputCurrencyValue) {
-		
-		if (inputCurrencyType.equals(getBaseCurrency()) && outputCurrencyType.equals(getCurrency()) ) {
-			  
-			exchangeRate = getExchangeRate(inputCurrencyValue);
-			
-		}
-
-		return exchangeRate; 
-	}
 	
-	@Override
-	public CurrencyUnit getBaseCurrency() {
-		
-		return inputCurrencyType;
-	}
-	
-	@Override
-	public CurrencyUnit getCurrency() {
-		
-		return outputCurrencyType;
-	}
-	
-	
-	@Override
-	public MonetaryAmount apply(MonetaryAmount inputCurrencyValue) {
-		
-		
-		outputCurrencyValue = ;
-		
-		
-		return outputCurrencyValue;
-	}
-	
-	
-	@Override
-	public ConversionContext getContext() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	
-
-	
-	
-
-	@Override
-	public NumberValue getFactor() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<ExchangeRate> getExchangeRateChain() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	
-
-	
-
-	@Override
-	public ExchangeRateProvider getExchangeRateProvider() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	
-	
-	
-	
-	
-	
-	//method for calculation of 'left to right' (divide) conversion
-/*	public MonetaryAmount leftToRightDivide() {
-		
-		
-		outputCurrencyValue = inputCurrencyValue.multiply(getFactor(inputCurrencyValue));
-		
-		return outputCurrencyValue;
-		
-	}
-	
-	*/
-	
-	/* //method for calculation of 'right to left' (multiply) conversion
-	public MonetaryAmount rightToLeftMultiply() {
-		
-		outputCurrencyValue = inputCurrencyValue.multiply((Number) getExchangeRateChain().get(1));
-		
-		return outputCurrencyValue;
-		
-	}
-	*/
-
-	
-
-}
